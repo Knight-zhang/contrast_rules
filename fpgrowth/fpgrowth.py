e9949ab7b054d1d6e1f1680b7657ddd589239c5d
Marharyta Aleksandrova <marharyta.aleksandrova@uni.lu>
2018-07-24 16:13:15 +0200

    correction of FP-growth (rules should be the same as for Apriori)

--- fpgrowth/fpgrowth.py
+++ fpgrowth/fpgrowth.py
@@ -168,6 +168,8 @@ class FPTree(object):
         if suffix is not None:
             # We are in a conditional tree.
             new_patterns = {}
+            # add the root first
+            new_patterns[tuple([suffix])] = self.root.count
             for key in patterns.keys():
                 new_patterns[tuple(sorted(list(key) + [suffix]))] = patterns[key]
 
@@ -247,6 +249,35 @@ class FPTree(object):
 
         return patterns
 
+    def to_string(self):
+        """
+        Get string representation of the tree
+        :return:
+        """
+        tree_structure_str = self.node_to_string(self.root, 0, is_add_children=True).rstrip()
+        return tree_structure_str
+
+    def node_to_string(self, node, tab_count=0, is_add_children=False):
+        """
+        Get String representation of the node
+        :param node:
+        :param tab_count:
+        :param is_add_children:
+        :return:
+        """
+        tabs_str = ''
+        for i in range(0, tab_count):
+            tabs_str += '\t'
+
+        node_str = tabs_str + str(node.value) + ': ' + str(node.count)
+
+        children_str = ''
+        if is_add_children:
+            for child_node in node.children:
+                children_str += '\n\t' + tabs_str + self.node_to_string(child_node, tab_count+1, True)
+
+        return node_str + children_str
+
 
 def find_frequent_patterns(transactions, support_threshold):
     """
@@ -275,7 +306,96 @@ def generate_association_rules(patterns, confidence_threshold, num_of_transactio
                 consequent = tuple(sorted(set(itemset) - antecedent_set))
                 consequent_set = set(consequent)
 
-                if antecedent in patterns:
+                lower_support = patterns[antecedent]
+                confidence = float(upper_support) / lower_support
+
+                if confidence >= confidence_threshold:
+                    # form antecedent and censequent in string format (the tuples are already sorted)
+                    consequent_str = ''
+                    for el in consequent:
+                        consequent_str += el + ','
+                    consequent_str = consequent_str[:-1]
+                    antecedent_str = ''
+                    for el in antecedent:
+                        antecedent_str += el + ','
+                    antecedent_str = antecedent_str[:-1]
+                    a_rule = {constants.LHS: antecedent_str, constants.RHS: consequent_str,
+                              constants.LHS_SET: antecedent_set, constants.RHS_SET: consequent_set,
+                              constants.LHS_SUPP_COUNT: lower_support, constants.RULE_SUPP_COUNT: upper_support,
+                              constants.LHS_SUPP: (float(lower_support) / num_of_transactions),
+                              constants.RULE_SUPP: (float(upper_support) / num_of_transactions),
+                              constants.RULE_CONF: confidence, constants.LINKS: ''}
+                    rules.append(a_rule)
+
+    return rules
+
+
+def generate_association_rules_with_one_item_consequent(patterns, confidence_threshold, num_of_transactions):
+    """
+    Given a set of frequent itemsets, return a dict
+    of association rules in the form
+    {(left): ((right), confidence)}
+    right contains only one item (is and itemset of size 1)
+    """
+    rules = []
+    for itemset in patterns.keys():
+        if len(itemset) > 1:
+            # we can't generate a rule from an itemset containing only 1 item, at least 2 are required
+            upper_support = patterns[itemset]
+
+            for consequent_el in itemset:
+                # consequent_el has only one elements, so it is already sorted
+                consequent = tuple([consequent_el])
+                consequent_set = set(consequent)
+                antecedent = tuple(sorted(set(itemset) - consequent_set))
+                antecedent_set = set(antecedent)
+
+                lower_support = patterns[antecedent]
+                confidence = float(upper_support) / lower_support
+
+                if confidence >= confidence_threshold:
+                    # form antecedent and censequent in string format (the tuples are already sorted)
+                    consequent_str = ''
+                    for el in consequent:
+                        consequent_str += el + ','
+                    consequent_str = consequent_str[:-1]
+                    antecedent_str = ''
+                    for el in antecedent:
+                        antecedent_str += el + ','
+                    antecedent_str = antecedent_str[:-1]
+                    a_rule = {constants.LHS: antecedent_str, constants.RHS: consequent_str,
+                              constants.LHS_SET: antecedent_set, constants.RHS_SET: consequent_set,
+                              constants.LHS_SUPP_COUNT: lower_support, constants.RULE_SUPP_COUNT: upper_support,
+                              constants.LHS_SUPP: (float(lower_support) / num_of_transactions),
+                              constants.RULE_SUPP: (float(upper_support) / num_of_transactions),
+                              constants.RULE_CONF: confidence, constants.LINKS: ''}
+                    rules.append(a_rule)
+    return rules
+
+
+def generate_classification_rules(patterns, confidence_threshold, num_of_transactions, class_values):
+    """
+    Given a set of frequent itemsets, return a dict
+    of association rules in the form
+    {(left): ((right), confidence)}
+    right contains only one item (is and itemset of size 1)
+    """
+    rules = []
+    for itemset in patterns.keys():
+        if len(itemset) > 1:
+            # we can't generate a rule from an itemset containing only 1 item, at least 2 are required
+            upper_support = patterns[itemset]
+
+            # only classification rules are required; that is only those itemsets should be considered,
+            # that have one of class values inside
+            for consequent_el in class_values:
+                if consequent_el in itemset:
+                    # consequent_el has only one elements, so it is already sorted
+                    consequent = tuple([consequent_el])
+                    consequent_set = set(consequent)
+                    antecedent = tuple(sorted(set(itemset) - consequent_set))
+                    antecedent_set = set(antecedent)
+
                     lower_support = patterns[antecedent]
                     confidence = float(upper_support) / lower_support
 
@@ -296,5 +416,6 @@ def generate_association_rules(patterns, confidence_threshold, num_of_transactio
                                   constants.RULE_SUPP: (float(upper_support) / num_of_transactions),
                                   constants.RULE_CONF: confidence, constants.LINKS: ''}
                         rules.append(a_rule)
-
+                    # class value was found, no other class values can be in the itemset ==> bread the loop
+                    break
     return rules
--- fpgrowth/fpgrowth_main.py
+++ fpgrowth/fpgrowth_main.py
@@ -6,12 +6,13 @@ sys.path.insert(0, '../util')
 import util_functions
 
 
-def run(transactions_file_name, min_supp_count, min_conf, output_file_name=None):
+def run(transactions_file_name, min_supp_count, min_conf, possible_class_values, output_file_name=None):
 
     transactions = util_functions.unzip_transactions_2(transactions_file_name)
     start = time.time()
     patterns = fpgrowth.find_frequent_patterns(transactions, min_supp_count)
-    rules = fpgrowth.generate_association_rules(patterns, min_conf, len(transactions))
+    #rules = fpgrowth.generate_association_rules_with_one_item_consequent(patterns, min_conf, len(transactions))
+    rules = fpgrowth.generate_classification_rules(patterns, min_conf, len(transactions), possible_class_values)
     end = time.time()
     print('Total elapsed {}'.format((end - start)))
     if output_file_name is not None:
@@ -28,9 +29,10 @@ def run(transactions_file_name, min_supp_count, min_conf, output_file_name=None)
 if __name__ == '__main__':
     path_name = '../data/'
     transactions_file_name = 'toMine_1_1.txt'
-    min_supp_count = 10
+    min_supp_count = 100
     min_conf = 0.7
+    possible_class_values = ["YES", "NO"]
     output_file_name = '../results/fpgrowth/{}_supp_{}_conf_{}.txt'.\
         format(transactions_file_name[:transactions_file_name.find('.txt')], min_supp_count,
                str(min_conf).replace('.', ''))
-    run(path_name + transactions_file_name, min_supp_count, min_conf, output_file_name)
+    run(path_name + transactions_file_name, min_supp_count, min_conf, possible_class_values, output_file_name)
